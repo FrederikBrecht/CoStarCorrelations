26b8e4d6bd5b23308e2505515b59dd6bd0e54fad
Ellypaca <59664766+Ellypaca@users.noreply.github.com>
2024-04-01 03:13:17 -0400

    Add files via upload
    Heavily referenced EX3

--- src/interface.py
+++ src/interface.py
@@ -14,7 +14,8 @@ from data import db_filter
 # FINAL VARIABLES
 METHODS = ["The average performance of a group of actors",
            "The best performing movie of a group of actors",
-           "The castmates of a particular actor"]
+           "The castmates of a particular actor",
+           "See a graph of all the movies and actors! (May take a while)"]
 
 
 BEST_MOVIE_TEXT = "The actors\' best movie together was: "
@@ -41,7 +42,7 @@ def run_collabs() -> None:
     """
     while True:
         _event, _values = COLLAB.read()
-        print(_event, _values)
+        # print(_event, _values)
 
         if _event == sg.WIN_CLOSED or _event == "Cancel":
             COLLAB["-COLLAB-"].update(value="The actors\' average score working together was: ")
@@ -51,7 +52,7 @@ def run_collabs() -> None:
         if _event == "Submit":
             actor_list = _values[0].split(", ")
 
-            print(actor_list)
+            # print(actor_list)
             try:
                 actor_id = [ACTOR_MOVIE_GRAPH.get_id(n) for n in actor_list]
             except ValueError:
@@ -71,7 +72,7 @@ def run_best_movie() -> None:
     """
     while True:
         _event, _values = BEST.read()
-        print(_event, _values)
+        # print(_event, _values)
 
         if _event == sg.WIN_CLOSED or _event == "Cancel":
             BEST["-MOVIE-"].update(value=BEST_MOVIE_TEXT)
@@ -90,7 +91,7 @@ def run_best_movie() -> None:
                 if best_movie_together != "/N":
                     best_movie_together = ACTOR_MOVIE_GRAPH.get_name(best_movie_together)
 
-                    print(best_movie_together)
+                    # print(best_movie_together)
                     BEST["-MOVIE-"].update(value=BEST_MOVIE_TEXT + best_movie_together)
 
                 else:
@@ -103,7 +104,7 @@ def run_find_castmates() -> None:
     """
     while True:
         _event, _values = CASTMATES.read()
-        print(_event, _values)
+        # print(_event, _values)
 
         if _event == sg.WIN_CLOSED or _event == "Cancel":
             update_castmates()
@@ -127,7 +128,7 @@ def run_find_castmates() -> None:
                                                                     int(_values["-MIN COLLABS-"]))
                     num_actors = len(team_list)
                     rows = num_actors // 5
-                    print(team_list)
+                    # print(team_list)
 
                     for row in range(min(rows, 5)):
                         team = ""
@@ -158,12 +159,85 @@ def update_castmates() -> None:
     """Helper for run_find_castmates to facilitate resetting the window.
 
     """
-    CASTMATES["-COSTARS-"].update(value="The casting team created in descending order is: ")
+    CASTMATES["-COSTARS-"].update(value=CASTMATES_TEXT)
     CASTMATES["-BLANK0-"].update(value="")
     CASTMATES["-BLANK1-"].update(value="")
     CASTMATES["-BLANK2-"].update(value="")
     CASTMATES["-BLANK3-"].update(value="")
     CASTMATES["-BLANK4-"].update(value="")
+
+
+# MODIFIED FROM ex3_visualization.py
+def visualize_graph(graph: datastructures.Graph, layout: str = 'spring_layout', max_vertices: int = 5000,
+                    output_file: str = '') -> None:
+    """Use plotly and networkx to visualize the given graph.
+
+    Optional arguments:
+        - layout: which graph layout algorithm to use
+        - max_vertices: the maximum number of vertices that can appear in the graph
+        - output_file: a filename to save the plotly image to (rather than displaying
+            in your web browser)
+    """
+    COLOUR_SCHEME = [
+        '#2E91E5', '#E15F99', '#1CA71C', '#FB0D0D', '#DA16FF', '#222A2A', '#B68100',
+        '#750D86', '#EB663B', '#511CFB', '#00A08B', '#FB00D1', '#FC0080', '#B2828D',
+        '#6C7C32', '#778AAE', '#862A16', '#A777F1', '#620042', '#1616A7', '#DA60CA',
+        '#6C4516', '#0D2A63', '#AF0038'
+    ]
+
+    LINE_COLOUR = 'rgb(210,210,210)'
+    VERTEX_BORDER_COLOUR = 'rgb(50, 50, 50)'
+    ACTOR_COLOUR = 'rgb(89, 205, 105)'
+    MOVIE_COLOUR = 'rgb(105, 89, 205)'
+
+    graph_nx = graph.to_networkx(max_vertices)
+
+    pos = getattr(nx, layout)(graph_nx)
+
+    x_values = [pos[k][0] for k in graph_nx.nodes]
+    y_values = [pos[k][1] for k in graph_nx.nodes]
+    labels = list(graph_nx.nodes)
+    names = [lab.name for lab in labels if type(lab) == datastructures.Actor or type(lab) == datastructures.Movie]
+
+    colours = [MOVIE_COLOUR if type(lab) == datastructures.Actor else ACTOR_COLOUR for lab in labels]
+
+    x_edges = []
+    y_edges = []
+    for edge in graph_nx.edges:
+        x_edges += [pos[edge[0]][0], pos[edge[1]][0], None]
+        y_edges += [pos[edge[0]][1], pos[edge[1]][1], None]
+
+    trace3 = Scatter(x=x_edges,
+                     y=y_edges,
+                     mode='lines',
+                     name='edges',
+                     line=dict(color=LINE_COLOUR, width=1),
+                     hoverinfo='none',
+                     )
+    trace4 = Scatter(x=x_values,
+                     y=y_values,
+                     mode='markers',
+                     name='nodes',
+                     marker=dict(symbol='circle-dot',
+                                 size=5,
+                                 color=colours,
+                                 line=dict(color=VERTEX_BORDER_COLOUR, width=0.5)
+                                 ),
+                     text=names,
+                     hovertemplate='%{text}',
+                     hoverlabel={'namelength': 0}
+                     )
+
+    data1 = [trace3, trace4]
+    fig = Figure(data=data1)
+    fig.update_layout({'showlegend': False})
+    fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
+    fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
+
+    if output_file == '':
+        fig.show()
+    else:
+        fig.write_image(output_file)
 
 
 # LAYOUTS============================================
@@ -218,7 +292,7 @@ CASTMATES = sg.Window("Finding Castmates!", CASTMATES_LAYOUT, margins=MARGINS)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = WINDOW.read()
-    print(event, values)
+    # print(event, values)
 
     if event == sg.WIN_CLOSED or event == "-CANCEL-":  # if user closes window or clicks cancel
         break
@@ -247,6 +321,10 @@ while True:
 
         run_find_castmates()
         WINDOW.un_hide()
+
+    if values[event] == "See a graph of all the movies and actors! (May take a while)":
+        visualize_graph(ACTOR_MOVIE_GRAPH)
+
 
 WINDOW.close()
 
